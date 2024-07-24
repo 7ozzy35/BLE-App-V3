b67541f60b32981e4174b1eb7b12c9543ca119ae
7ozzy35 <84852769+7ozzy35@users.noreply.github.com>
2024-07-24 15:27:13 +0300

    keke eklendi

---
+++ keke.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+const keke = () => {
+  return (
+    <View>
+
+      <TouchableOpacity>
+        <Text>ozan</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default keke
