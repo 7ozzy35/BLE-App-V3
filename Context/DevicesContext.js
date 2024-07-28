ee17b51220822a25412f6021c9b78eedb7109207
Umutcan önal <100920169+CanUmut62@users.noreply.github.com>
2024-07-28 17:26:04 +0300

    auto bağlantı ve kapı aç butonu aktifliği güncellendi cardControl eklendi

--- Context/DevicesContext.js
+++ Context/DevicesContext.js
@@ -1,32 +1,58 @@
 import React, { useState, createContext, useCallback } from 'react';
 import { BleManager } from 'react-native-ble-plx';
 import { Buffer } from 'buffer';
-import { count } from 'console';
 
 export const DeviceContext = createContext();
-const bleManager = new BleManager();
+let bleManager = new BleManager();
 
 export const DeviceProvider = ({ children }) => {
   const [connectedDevice, setConnectedDevice] = useState(null);
   const [disconnectMessage, setDisconnectMessage] = useState('');
   const [disconnectButtonVisible, setDisconnectButtonVisible] = useState(false);
   const myId = "12:6C:14:38:F5:40"; // Replace with your device ID
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const resetBleManager = () => {
+    bleManager.destroy();
+    setTimeout(() => {
+      bleManager = new BleManager();
+      console.log('BleManager reset');
+    }, 1000);
+  };
+
+  const disconnectDevice = async (manual = true) => {
+    if (connectedDevice) {
+      try {
+        if (bleManager.state !== 'destroyed') {
+          await bleManager.cancelDeviceConnection(connectedDevice.id);
+          console.log('Disconnected from device');
+          setConnectedDevice(null);
+          if (manual) {
+            setDisconnectMessage('Kapı bağlantısı manuel olarak kesildi');
+          }
+          setDisconnectButtonVisible(false);
+        } else {
+          console.error('BleManager is destroyed and cannot disconnect');
+        }
+      } catch (error) {
+        console.error('Failed to disconnect:', error);
+      }
+    }
+  };
 
   const handleDoorOpen = useCallback(async () => {
+    setIsButtonDisabled(true);
     try {
       const device = await bleManager.connectToDevice(myId);
       await device.discoverAllServicesAndCharacteristics();
       console.log("Device connected:", device);
-      await setConnectedDevice(device);
-      await setDisconnectMessage('');
-      await setDisconnectButtonVisible(true);
+      setConnectedDevice(device);
+      setDisconnectMessage('');
+      setDisconnectButtonVisible(true);
 
       const data = '<1:4:1>';
       await sendDataToDevice(device, '0000ffe0-0000-1000-8000-00805f9b34fb', '0000ffe1-0000-1000-8000-00805f9b34fb', data);
       console.log('Door open command sent');
-
-      // Automatically disconnect after 10 seconds
-
       const disconnectDevice2= async () => {
         if (device) {
           try {
@@ -46,15 +72,77 @@ export const DeviceProvider = ({ children }) => {
 
       const autoDisconnectTimeout = setTimeout(async () => {
         await disconnectDevice2();
-      }, 5000);
+      }, 2500);
 
-      // Store the timeout id to clear it if manual disconnect occurs
       device.autoDisconnectTimeout = autoDisconnectTimeout;
     } catch (error) {
       console.log('Failed to open door:', error);
+      if (error.message.includes('BleManager was destroyed')) {
+        console.log("BLE Manager destroyed, resetting...");
+        resetBleManager();
+      }
     }
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 3000);
   }, [myId]);
 
+  
+  const CardControl = useCallback(async (data) => {
+    setIsButtonDisabled(true);
+    try {
+      const device = await bleManager.connectToDevice(myId);
+      await device.discoverAllServicesAndCharacteristics();
+      console.log("Device connected:", device);
+      setConnectedDevice(device);
+      setDisconnectMessage('');
+      setDisconnectButtonVisible(true);
+  
+      await sendDataToDevice(device, '0000ffe0-0000-1000-8000-00805f9b34fb', '0000ffe1-0000-1000-8000-00805f9b34fb', data);
+      console.log('Command sent');
+  
+      // Veriyi okuyacağımız karakteristik UUID'yi belirleyin
+      const serviceUUID = '0000fee7-0000-1000-8000-00805f9b34fb';
+      const characteristicUUID = '0000ffe1-0000-1000-8000-00805f9b34fb';
+  
+      // Cihazdan veri okuma
+      const readData = await device.readCharacteristicForService(serviceUUID, characteristicUUID);
+      const decodedData = Buffer.from(readData.value, 'base64').toString('utf-8');
+      console.log('Received data:', decodedData);
+  
+      const disconnectDevice2 = async () => {
+        if (device) {
+          try {
+            if (bleManager.state !== 'destroyed') {
+              await bleManager.cancelDeviceConnection(myId);
+              console.log('Disconnected from device');
+              setConnectedDevice(null);
+            } else {
+              console.error('BleManager is destroyed and cannot disconnect');
+            }
+          } catch (count) {
+            console.log('bağlantı koptu', count);
+          }
+        }
+      };
+  
+      const autoDisconnectTimeout = setTimeout(async () => {
+        await disconnectDevice2();
+      }, 2500);
+  
+      device.autoDisconnectTimeout = autoDisconnectTimeout;
+    } catch (error) {
+      console.log('Failed to open door:', error);
+      if (error.message.includes('BleManager was destroyed')) {
+        console.log("BLE Manager destroyed, resetting...");
+        resetBleManager();
+      }
+    }
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 3000);
+  }, [myId]);
+  
   const sendDataToDevice = async (device, serviceUUID, characteristicUUID, data) => {
     try {
       await device.writeCharacteristicWithResponseForService(
@@ -67,33 +155,10 @@ export const DeviceProvider = ({ children }) => {
       console.error('Failed to send data:', error);
     }
   };
-
-
-  // ŞİMDİLİK GEREKSİZ
-
-
-  const disconnectDevice = useCallback(async () => {
-    console.log("31313131",connectedDevice)
-    if (connectedDevice) {
-      try {
-       // Geçici olarak erişilebilirlik kontrolü
-       if (bleManager.state !== 'destroyed') {
-        await bleManager.cancelDeviceConnection("12:6C:14:38:F5:40");
-        console.log('Disconnected from device');
-        setConnectedDevice(null);
-      } else {
-        console.error('BleManager is destroyed and cannot disconnect');
-      }
-    } catch (error) {
-      console.error('Failed to disconnect2:', error);
-      }
-    } else {
-      console.log('No device connected');
-    }
-  }, [connectedDevice]);
+  
 
   return (
-    <DeviceContext.Provider value={{ connectedDevice, setConnectedDevice, handleDoorOpen, sendDataToDevice, disconnectDevice, disconnectMessage, disconnectButtonVisible, setDisconnectButtonVisible, setDisconnectMessage }}>
+    <DeviceContext.Provider value={{ connectedDevice, setConnectedDevice, handleDoorOpen, sendDataToDevice, disconnectDevice, disconnectMessage, disconnectButtonVisible, setDisconnectButtonVisible, setDisconnectMessage, isButtonDisabled,CardControl }}>
       {children}
     </DeviceContext.Provider>
   );
