31c178a5d761d1c9359aec26eda43db2590f8b62
Umutcan önal <100920169+CanUmut62@users.noreply.github.com>
2024-08-01 16:01:22 +0300

    cihaz bilgileri ve komutları buraya eklenecek

---
+++ Component/DeviceInfo.js
@@ -0,0 +1,5 @@
+export const myId = "12:6C:14:38:54:50";
+export const serviceUUID = '0000ffe0-0000-1000-8000-00805f9b34fb';
+export const characteristicUUID = '0000ffe1-0000-1000-8000-00805f9b34fb';
+
+export const OpenDoorComment = "<1:4:3>"
