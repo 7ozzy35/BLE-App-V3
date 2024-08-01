418c15e439749fdc60368ced2c28b805c078ea5f
Umutcan önal <100920169+CanUmut62@users.noreply.github.com>
2024-08-01 16:08:20 +0300

    rfidNAme eklendi

--- Component/DeviceInfo.js
+++ Component/DeviceInfo.js
@@ -2,4 +2,5 @@ export const myId = "12:6C:14:38:54:50";
 export const serviceUUID = '0000ffe0-0000-1000-8000-00805f9b34fb';
 export const characteristicUUID = '0000ffe1-0000-1000-8000-00805f9b34fb';
 
-export const OpenDoorComment = "<1:4:3>"
+export const OpenDoorComment = "<1:4:3>";
+export  const rfidName ='12345';
