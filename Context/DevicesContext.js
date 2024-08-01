46d33a717f6ae410ba0b5290daf2ce1f9ff5a144
Umutcan önal <100920169+CanUmut62@users.noreply.github.com>
2024-08-01 20:59:54 +0300

    veri göndermek ve artları aktarmakve silmek  için 3 fonksiyon oluturuldu

--- Context/DevicesContext.js
+++ Context/DevicesContext.js
@@ -93,44 +93,41 @@ export const DeviceProvider = ({ children }) => {
       setIsButtonDisabled(false);
     }, 2500);
   }, [myId]);
-
-
- 
-
- 
-
-  const CardControl = useCallback(async () => {
+  const sendComment = useCallback(async (sendData) => {
     setIsButtonDisabled(true);
     try {
-      // Cihaza bağlan
       const device = await bleManager.connectToDevice(myId);
       await device.discoverAllServicesAndCharacteristics();
       console.log("Device connected:", device);
-      
-  
-      // Veri yazma
-    await device.writeCharacteristicWithResponseForService(
-      '0000ffe0-0000-1000-8000-00805f9b34fb',  // Servis UUID
-      '0000ffe1-0000-1000-8000-00805f9b34fb',  // Karakteristik UUID
-      "<1:7>",
-      Buffer.from("<1:7>").toString('base64')  // Gönderilecek veri
-    );
-  
-    // Yanıtları dinleme
-    device.monitorCharacteristicForService(
-      '0000ffe0-0000-1000-8000-00805f9b34fb', // Servis UUID
-      '0000ffe1-0000-1000-8000-00805f9b34fb',   // Karakteristik UUID
-      (error, characteristic) => {
-        if (error) {
-          console.log('Error:', error.message);
-          return;
+      setConnectedDevice(device);
+      setDisconnectMessage('');
+      setDisconnectButtonVisible(true);
+
+      // const data = '<1:34625:4:3>';
+      await sendDataToDevice(device,serviceUUID, characteristicUUID, sendData);
+      console.log('Door open command sent');
+      const disconnectDevice2= async () => {
+        if (device) {
+          try {
+           // Geçici olarak erişilebilirlik kontrolü
+           if (bleManager.state !== 'destroyed') {
+            await bleManager.cancelDeviceConnection(myId);
+            console.log('Disconnected from device');
+            setConnectedDevice(null);
+          } else {
+            console.error('BleManager is destroyed and cannot disconnect');
+          }
+        } catch (count) {
+          console.log('bağlantı koptu',count);
+          }
         }
-        // Yanıt verisi burada
-        const response = characteristic.value;
-        console.log('Response:', response);
-      }
-    );
-  
+      };
+
+      const autoDisconnectTimeout = setTimeout(async () => {
+        await disconnectDevice2();
+      }, 5000);
+
+      device.autoDisconnectTimeout = autoDisconnectTimeout;
     } catch (error) {
       console.log('Failed to open door:', error);
       if (error.message.includes('BleManager was destroyed')) {
@@ -138,10 +135,174 @@ export const DeviceProvider = ({ children }) => {
         resetBleManager();
       }
     }
-    
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 2500);
   }, [myId]);
-  
-  
+
+
+  const sendCardData = useCallback(async (usersList) => {
+    setIsButtonDisabled(true);
+    try {
+      const device = await bleManager.connectToDevice(myId);
+      await device.discoverAllServicesAndCharacteristics();
+      console.log("Device connected:", device);
+      setConnectedDevice(device);
+      setDisconnectMessage('');
+      setDisconnectButtonVisible(true);
+
+
+      // const usersList = [
+      //   "34621",
+      //   "34622",
+      //   "34623",
+      //   "34624",
+      //   "34625",
+      //   "34646",
+      //   "34647",
+      //   "34648",
+      //   "34649",
+      //   "34650",
+      //   "34651",
+      //   "34652",
+        
+        
+
+
+      // ]
+      
+ 
+     await  usersList.forEach(element => {
+        sendDataToDevice(device,serviceUUID, characteristicUUID, `<1:8:${element["Kart No"]}:001>`);
+       });
+        
+      
+
+      // const data = '<1:34625:4:3>';
+     
+        
+        
+      
+      
+      console.log('Door open command sent');
+      const disconnectDevice2= async () => {
+        if (device) {
+          try {
+           // Geçici olarak erişilebilirlik kontrolü
+           if (bleManager.state !== 'destroyed') {
+            await bleManager.cancelDeviceConnection(myId);
+            console.log('Disconnected from device');
+            setConnectedDevice(null);
+          } else {
+            console.error('BleManager is destroyed and cannot disconnect');
+          }
+        } catch (count) {
+          console.log('bağlantı koptu',count);
+          }
+        }
+      };
+
+      const autoDisconnectTimeout = setTimeout(async () => {
+        await disconnectDevice2();
+      }, 5000);
+
+      device.autoDisconnectTimeout = autoDisconnectTimeout;
+    } catch (error) {
+      console.log('Failed to open door:', error);
+      if (error.message.includes('BleManager was destroyed')) {
+        console.log("BLE Manager destroyed, resetting...");
+        resetBleManager();
+      }
+    }
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 2500);
+  }, [myId]);
+  const sendDeleteCardData = useCallback(async (usersList) => {
+    setIsButtonDisabled(true);
+    try {
+      const device = await bleManager.connectToDevice(myId);
+      await device.discoverAllServicesAndCharacteristics();
+      console.log("Device connected:", device);
+      setConnectedDevice(device);
+      setDisconnectMessage('');
+      setDisconnectButtonVisible(true);
+
+
+      // const usersList = [
+      //   "34621",
+      //   "34622",
+      //   "34623",
+      //   "34624",
+      //   "34625",
+      //   "34646",
+      //   "34647",
+      //   "34648",
+      //   "34649",
+      //   "34650",
+      //   "34651",
+      //   "34652",
+        
+        
+
+
+      // ]
+      
+ 
+     await  usersList.forEach(element => {
+        sendDataToDevice(device,serviceUUID, characteristicUUID, `<1:9:${element["Kart No"]}>`);
+       });
+        
+      
+
+      // const data = '<1:34625:4:3>';
+     
+        
+        
+      
+      
+      console.log('Door open command sent');
+      const disconnectDevice2= async () => {
+        if (device) {
+          try {
+           // Geçici olarak erişilebilirlik kontrolü
+           if (bleManager.state !== 'destroyed') {
+            await bleManager.cancelDeviceConnection(myId);
+            console.log('Disconnected from device');
+            setConnectedDevice(null);
+          } else {
+            console.error('BleManager is destroyed and cannot disconnect');
+          }
+        } catch (count) {
+          console.log('bağlantı koptu',count);
+          }
+        }
+      };
+
+      const autoDisconnectTimeout = setTimeout(async () => {
+        await disconnectDevice2();
+      }, 5000);
+
+      device.autoDisconnectTimeout = autoDisconnectTimeout;
+    } catch (error) {
+      console.log('Failed to open door:', error);
+      if (error.message.includes('BleManager was destroyed')) {
+        console.log("BLE Manager destroyed, resetting...");
+        resetBleManager();
+      }
+    }
+    setTimeout(() => {
+      setIsButtonDisabled(false);
+    }, 2500);
+  }, [myId]);
+
+
+ 
+
+ 
+
+ 
+  // data gönderir ve gelen cevapları dinler
   const sendDataToDevice = async (device, serviceUUID, characteristicUUID, data) => {
     try {
       const characteristic = await device.writeCharacteristicWithResponseForService(
@@ -173,7 +334,7 @@ export const DeviceProvider = ({ children }) => {
   
 
   return (
-    <DeviceContext.Provider value={{ kartNo, setKartNo,userİnfo, setUserİnfo,userToken, setUserToken,connectedDevice, setConnectedDevice, handleDoorOpen, sendDataToDevice, disconnectDevice, disconnectMessage, disconnectButtonVisible, setDisconnectButtonVisible, setDisconnectMessage, isButtonDisabled,CardControl }}>
+    <DeviceContext.Provider value={{ sendDeleteCardData,sendCardData,kartNo, setKartNo,userİnfo, setUserİnfo,userToken, setUserToken,connectedDevice, setConnectedDevice, handleDoorOpen, sendDataToDevice, disconnectDevice, disconnectMessage, disconnectButtonVisible, setDisconnectButtonVisible, setDisconnectMessage, isButtonDisabled }}>
       {children}
     </DeviceContext.Provider>
   );
