817f262aa152a80c6afbbd89358fef6eca0e7008
Umutcan önal <100920169+CanUmut62@users.noreply.github.com>
2024-08-01 16:02:25 +0300

    veri cevap dönme eklendi düzenleme gerekli

--- Context/DevicesContext.js
+++ Context/DevicesContext.js
@@ -1,6 +1,7 @@
 import React, { useState, createContext, useCallback } from 'react';
 import { BleManager } from 'react-native-ble-plx';
 import { Buffer } from 'buffer';
+import { myId,OpenDoorComment,characteristicUUID,serviceUUID } from '../Component/DeviceInfo'
 
 export const DeviceContext = createContext();
 let bleManager = new BleManager();
@@ -13,7 +14,8 @@ export const DeviceProvider = ({ children }) => {
   const [connectedDevice, setConnectedDevice] = useState(null);
   const [disconnectMessage, setDisconnectMessage] = useState('');
   const [disconnectButtonVisible, setDisconnectButtonVisible] = useState(false);
-  const myId = "12:6C:14:38:F5:40"; // Replace with your device ID
+  // const myId = "12:6C:14:38:54:50"; 
+  // Replace with your device ID
   const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
   const resetBleManager = () => {
@@ -54,15 +56,15 @@ export const DeviceProvider = ({ children }) => {
       setDisconnectMessage('');
       setDisconnectButtonVisible(true);
 
-      const data = '<1:4:1>';
-      await sendDataToDevice(device, '0000ffe0-0000-1000-8000-00805f9b34fb', '0000ffe1-0000-1000-8000-00805f9b34fb', data);
+      // const data = '<1:34625:4:3>';
+      await sendDataToDevice(device,serviceUUID, characteristicUUID, OpenDoorComment);
       console.log('Door open command sent');
       const disconnectDevice2= async () => {
         if (device) {
           try {
            // Geçici olarak erişilebilirlik kontrolü
            if (bleManager.state !== 'destroyed') {
-            await bleManager.cancelDeviceConnection("12:6C:14:38:F5:40");
+            await bleManager.cancelDeviceConnection(myId);
             console.log('Disconnected from device');
             setConnectedDevice(null);
           } else {
@@ -88,53 +90,46 @@ export const DeviceProvider = ({ children }) => {
     }
     setTimeout(() => {
       setIsButtonDisabled(false);
-    }, 3000);
+    }, 2500);
   }, [myId]);
 
-  
-  const CardControl = useCallback(async (data) => {
+
+ 
+
+ 
+
+  const CardControl = useCallback(async () => {
     setIsButtonDisabled(true);
     try {
+      // Cihaza bağlan
       const device = await bleManager.connectToDevice(myId);
       await device.discoverAllServicesAndCharacteristics();
       console.log("Device connected:", device);
-      setConnectedDevice(device);
-      setDisconnectMessage('');
-      setDisconnectButtonVisible(true);
+      
   
-      await sendDataToDevice(device, '0000ffe0-0000-1000-8000-00805f9b34fb', '0000ffe1-0000-1000-8000-00805f9b34fb', data);
-      console.log('Command sent');
+      // Veri yazma
+    await device.writeCharacteristicWithResponseForService(
+      '0000ffe0-0000-1000-8000-00805f9b34fb',  // Servis UUID
+      '0000ffe1-0000-1000-8000-00805f9b34fb',  // Karakteristik UUID
+      "<1:7>",
+      Buffer.from("<1:7>").toString('base64')  // Gönderilecek veri
+    );
   
-      // Veriyi okuyacağımız karakteristik UUID'yi belirleyin
-      const serviceUUID = '0000ffe0-0000-1000-8000-00805f9b34fb';
-      const characteristicUUID = '0000ffe1-0000-1000-8000-00805f9b34fb';
-  
-      // Cihazdan veri okuma
-      const readData = await device.readCharacteristicForService(serviceUUID, characteristicUUID);
-      const decodedData = Buffer.from(readData.value, 'base64').toString('utf-8');
-      console.log('Received data:', decodedData);
-  
-      const disconnectDevice2 = async () => {
-        if (device) {
-          try {
-            if (bleManager.state !== 'destroyed') {
-              await bleManager.cancelDeviceConnection(myId);
-              console.log('Disconnected from device');
-              setConnectedDevice(null);
-            } else {
-              console.error('BleManager is destroyed and cannot disconnect');
-            }
-          } catch (count) {
-            console.log('bağlantı koptu', count);
-          }
+    // Yanıtları dinleme
+    device.monitorCharacteristicForService(
+      '0000ffe0-0000-1000-8000-00805f9b34fb', // Servis UUID
+      '0000ffe1-0000-1000-8000-00805f9b34fb',   // Karakteristik UUID
+      (error, characteristic) => {
+        if (error) {
+          console.log('Error:', error.message);
+          return;
         }
-      };
+        // Yanıt verisi burada
+        const response = characteristic.value;
+        console.log('Response:', response);
+      }
+    );
   
-      const autoDisconnectTimeout = setTimeout(async () => {
-        await disconnectDevice2();
-      }, 2500);
-  
-      device.autoDisconnectTimeout = autoDisconnectTimeout;
     } catch (error) {
       console.log('Failed to open door:', error);
       if (error.message.includes('BleManager was destroyed')) {
@@ -142,10 +137,9 @@ export const DeviceProvider = ({ children }) => {
         resetBleManager();
       }
     }
-    setTimeout(() => {
-      setIsButtonDisabled(false);
-    }, 3000);
+    
   }, [myId]);
+  
   
   const sendDataToDevice = async (device, serviceUUID, characteristicUUID, data) => {
     try {
@@ -154,11 +148,27 @@ export const DeviceProvider = ({ children }) => {
         characteristicUUID,
         Buffer.from(data).toString('base64')
       );
-      console.log('Data sent:', characteristic);
+
+      device.monitorCharacteristicForService(
+        serviceUUID,  // Servis UUID
+        characteristicUUID,  // Karakteristik UUID
+        (error, characteristic) => {
+          if (error) {
+            console.log('Error:', error.message);
+            return;
+          }
+          // Yanıt verisi burada
+          const response = characteristic.value;
+          console.log('Response:', Buffer.from(response, 'base64').toString('utf-8'));
+        }
+      );
+     
     } catch (error) {
       console.error('Failed to send data:', error);
     }
   };
+
+  
   
 
   return (
